55a96626d992cdffb7481b284df4ee951c292eac
importamt <dlaqhxoa@gmail.com>
2021-08-13 11:27:25 +0900

    add Storybook files
    간단한 UI 테스트를 위해 스토리북을 사용하고자 합니다.

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+const {configure} = require("@storybook/react");
+module.exports = {
+    core: {
+        builder: "webpack5",
+    },
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials"
+    ],
+    typescript: {
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            compilerOptions: {
+                allowSyntheticDefaultImports: false,
+                esModuleInterop: false,
+            },
+        }
+    }
+}
