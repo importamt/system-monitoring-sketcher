e12bbc9317de8cec3ba8799f7c42c138fd16fcc3
importamt <dlaqhxoa@gmail.com>
2021-08-13 18:06:18 +0900

    mod initialize

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,39 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import styled from "styled-components";
+import {Modifying, Monitoring} from "./pages";
+import {GlobalStyles} from "./styles";
 
-interface SystemMonitoringSketcherOptions {
-
-
+interface IStyledSystemMonitoringSketcher {
     width: number | string,
     height: number | string,
-
 }
 
-const SystemMonitoringSketcher = (elementId: string, sketcherOptions: SystemMonitoringSketcherOptions) => {
-    console.log("Props : ", elementId, sketcherOptions)
-    ReactDOM.render(<h1>HELLO BOTEM : {elementId}</h1>, document.getElementById(elementId))
+export interface SystemMonitoringSketcherOptions extends IStyledSystemMonitoringSketcher {
+    isMonitoring: boolean
+}
+
+const SystemMonitoringSketcher = (elementId: string, {
+    width,
+    height,
+    isMonitoring,
+}: SystemMonitoringSketcherOptions) => {
+    const calculatedWidth = typeof width === 'number' ? `${width}px` : width ? width : '100%'
+    const calculatedHeight = typeof height === 'number' ? `${height}px` : height ? height : '100%'
+
+    ReactDOM.render(
+        <StyledSystemMonitoringSketcher
+            width={calculatedWidth} height={calculatedHeight}>
+            <GlobalStyles/>
+            {isMonitoring ? <Monitoring/> : <Modifying/>}
+        </StyledSystemMonitoringSketcher>
+        , document.getElementById(elementId))
     return 0
 }
+
+const StyledSystemMonitoringSketcher = styled.main<IStyledSystemMonitoringSketcher>`
+  width: ${({width}) => width};
+  height: ${({height}) => height};
+`
 
 export default SystemMonitoringSketcher
