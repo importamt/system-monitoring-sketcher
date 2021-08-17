2ea5f04678a9e062eb3e223e716e79f2f6311b93
importamt <dlaqhxoa@gmail.com>
2021-08-17 17:09:09 +0900

    mod storybook preview; add decorators for [Styled-components:GlobalStyles], [react-dnd: DndProvider]
    모든 *.stories.tsx 파일에 위 내용 적용하느라 귀찮았었는데 이런 방법이 있었다.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import {GlobalStyles} from "../src/styles";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {DndProvider} from "react-dnd";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: {argTypesRegex: "^on[A-Z].*"},
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
+
+export const decorators = [
+    Story => (
+        <DndProvider backend={HTML5Backend}>
+            <GlobalStyles/>
+            <Story/>
+        </DndProvider>
+    )
+]
