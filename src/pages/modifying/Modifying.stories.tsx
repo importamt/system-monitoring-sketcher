0ce2b55cfb36efa9820b7418619a6244a694fd57
importamt <dlaqhxoa@gmail.com>
2021-08-17 17:20:34 +0900

    mod Modifying page to show its components.
    기본적인 동작은 하게끔 작업을 해놨다.
    
    storybook을 활용해서 FHD, HD 등 각종 화면의 반응형 동작여부 테스트가 쉬울듯 하다.

--- src/pages/modifying/Modifying.stories.tsx
+++ src/pages/modifying/Modifying.stories.tsx
@@ -1,13 +1,22 @@
 import {IModifying, Modifying} from "./Modifying";
 import {Meta, Story} from "@storybook/react";
+import {IResolutionForStory, RESOLUTION_FHD, RESOLUTION_HD} from "../../utils";
 
 export default {
     component: Modifying,
     title: 'Pages/Modifying'
 } as Meta
 
-const Template: Story<IModifying> = args => <div style={{width: '1280px', height: '720px'}}>
-    <Modifying {...args}/>
+const Template: Story<IModifying & IResolutionForStory> = args => <div style={args.resolution}>
+    <Modifying/>
 </div>
 
-export const Default = Template.bind({})
+export const Hd = Template.bind({})
+Hd.args = {
+    resolution: RESOLUTION_HD
+}
+
+export const Fhd = Template.bind({})
+Fhd.args = {
+    resolution: RESOLUTION_FHD
+}
--- src/pages/modifying/Modifying.tsx
+++ src/pages/modifying/Modifying.tsx
@@ -3,7 +3,10 @@
  */
 import styled from "styled-components";
 import {Diagram, Setting} from "../../components/templates";
-import {GlobalStyles} from "../../styles";
+import {useState} from "react";
+import {System} from "../../store/system";
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 export interface IModifying {
 
@@ -12,16 +15,37 @@ export interface IModifying {
 export const Modifying = ({}: IModifying) => {
 
     //Get Systems
-
-    //Get Connections
+    const [systems, setSystems] = useState<System[]>([
+        {
+            id: 'abc1',
+            name: 'HELLO1',
+            url: 'https://google.com',
+            x: 10,
+            y: 50,
+            isAssigned: true
+        }, {
+            id: 'abc2',
+            name: 'HELLO2',
+            url: 'https://google.com',
+            x: 10,
+            y: 50,
+            isAssigned: true
+        }, {
+            id: 'abc3',
+            name: 'HELLO3',
+            url: 'https://google.com',
+            x: 10,
+            y: 50,
+            isAssigned: true
+        },
+    ])
+    //Get Links
 
     return <StyledModifying>
-        <GlobalStyles/>
-
-        <Setting/>
-        <Diagram>
-            <></>
-        </Diagram>
+        <DndProvider backend={HTML5Backend}>
+            <Setting systems={systems}/>
+            <Diagram systems={systems}/>
+        </DndProvider>
     </StyledModifying>
 }
 
