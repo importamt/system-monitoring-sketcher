92dacc66c6526be8448580c8d559fea12dd8855a
importamt <dlaqhxoa@gmail.com>
2021-08-19 17:04:11 +0900

    mod SystemListItem's width ...
    내용이 길어지만 줄이 자동으로 넘어가서 그러지 않도록 ellipsis인가 그거 추가했음

--- src/components/molecules/system-list-item/SystemListItem.tsx
+++ src/components/molecules/system-list-item/SystemListItem.tsx
@@ -1,4 +1,4 @@
-import { useDrag } from "react-dnd";
+import {useDrag} from "react-dnd";
 import styled from "styled-components";
 import {System} from "../../../store/system";
 
@@ -19,14 +19,15 @@ export const SystemListItem = ({system}: ISystemListItem) => {
 
     return <StyledSystemListItem system={system} ref={drag}>
         <h1>{system?.name}</h1>
-        <small>{system?'ID: ' + system.id:null}</small>
-        <small>{system?'URL: ' + system.url:null}</small>
+        <small>{system ? 'ID: ' + system.id : null}</small>
+        <small>{system ? 'URL: ' + system.url : null}</small>
     </StyledSystemListItem>
 }
 
 const StyledSystemListItem = styled.li<ISystemListItem>`
   cursor: pointer;
-  
+
+  min-width: 100px;
   width: 100%;
   min-height: 100px;
   height: 10%;
@@ -39,27 +40,28 @@ const StyledSystemListItem = styled.li<ISystemListItem>`
   align-items: center;
   justify-content: center;
   gap: 5px;
-  
+
   & > h1, & > small {
     width: 80%;
     min-height: 15px;
     border-radius: 5px;
     background: ${({system}) => system ? 'transparent' : '#c8bdbd'};
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    text-align: center;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   & > h1 {
     height: 20%;
   }
-  
+
   & > small {
-    width: 60%;
+    width: 70%;
     height: 15%;
     justify-content: flex-start;
   }
-  
+
 `
 
