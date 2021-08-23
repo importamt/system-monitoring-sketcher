3c0d87054099ea7be14bb2ff6b32aab73a81a915
importamt <dlaqhxoa@gmail.com>
2021-08-23 18:18:26 +0900

    mod storybook preview to add Redux store whole preview stories...

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import {GlobalStyles} from "../src/styles";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import {DndProvider} from "react-dnd";
+import {createStore} from "../src/store";
+import {Provider} from "react-redux";
 
 export const parameters = {
     actions: {argTypesRegex: "^on[A-Z].*"},
@@ -11,12 +13,14 @@ export const parameters = {
         },
     },
 }
-
+const store = createStore()
 export const decorators = [
     Story => (
-        <DndProvider backend={HTML5Backend}>
-            <GlobalStyles/>
-            <Story/>
-        </DndProvider>
+        <Provider store={store}>
+            <DndProvider backend={HTML5Backend}>
+                <GlobalStyles/>
+                <Story/>
+            </DndProvider>
+        </Provider>
     )
 ]
