db2262506f4b2ca0a21cc506d108508df017455c
importamt <dlaqhxoa@gmail.com>
2021-08-23 18:31:14 +0900

    add root store ...
    전에도 그랬지만, reducer(store)는 domain별, 그리고 view 하위의 화면별로 분류할 것이다

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,34 @@
-export {System} from './system'
+import {combineReducers, configureStore, EnhancedStore} from "@reduxjs/toolkit";
+import {all, fork} from "@redux-saga/core/effects";
+import createSagaMiddleware from "redux-saga";
+import {SystemReducer, SystemSaga} from "./system";
+
+// const viewReducer = combineReducers({})
+const rootReducer = combineReducers({
+    // view: viewReducer,
+    system: SystemReducer,
+})
+const rootSaga = function* () {
+    yield all([
+        fork(SystemSaga)
+    ])
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export const createStore = (): EnhancedStore<RootState> => {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = configureStore({
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(sagaMiddleware),
+        devTools: process.env.NODE_ENV !== "production",
+    })
+    sagaMiddleware.run(rootSaga)
+    return store
+}
+
+export {
+    System,
+    SystemReducer,
+    SystemSaga,
+} from './system'
