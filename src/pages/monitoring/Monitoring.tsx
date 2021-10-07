9abed875d0c66d094574d1d5b1c1dd2365f13f40
importamt <dlaqhxoa@gmail.com>
2021-10-07 18:27:22 +0900

    add monitoring ...
    checks를 사용하고 setting부가 없다는게 Modifying과의 차이점

--- src/pages/monitoring/Monitoring.tsx
+++ src/pages/monitoring/Monitoring.tsx
@@ -1,6 +1,33 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useMount} from "../../hooks";
+import {retrieveSystemsRequest} from "../../store/system";
+import {retrieveLinksRequest} from "../../store/link";
+import {retrieveChecksRequest} from "../../store/check";
+import {RootState} from "../../store";
+import {Diagram} from "../../components/templates";
+import styled from "styled-components";
 
 
 export const Monitoring = () => {
-    return null
+    const dispatch = useDispatch()
+    useMount(() => {
+        dispatch(retrieveSystemsRequest())
+        dispatch(retrieveLinksRequest())
+        dispatch(retrieveChecksRequest())
+    })
+
+    const systems = useSelector((state: RootState) => state.system.systems)
+    const links = useSelector((state: RootState) => state.link.links)
+    const checks = useSelector((state: RootState) => state.check.checks)
+
+    return <StyledMonitoring>
+        <Diagram systems={systems?.filter(system => system.isAssigned === true)} links={links} checks={checks}/>
+    </StyledMonitoring>
 }
+
+const StyledMonitoring = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
 
